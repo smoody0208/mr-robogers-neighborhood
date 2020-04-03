99f9abc7fa0525786dd7b2546c0762dadd2944a5
Spencer Moody <spencer.moody@outlook.com>
2020-04-03 09:33:31 -0700

    Add content to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,19 @@ function robogersNeighborhood(message) {
   if (number<0) {
     alert("Please Enter a Positive Number");
     return "";
+    console.log(message);
   }
 
   for(var i = 0; i<= message; i++){
     if (i === 3) {
       result.push("won't you be my neighbor");
-    } else if (i === )
+    } else if (i === 1) {
+      result.push("beep");
+    } else if (i === 2) {
+      result.push("ping");
+    } else {
+      result.push(i);
+    }
   }
-}
+  return result;
+};
