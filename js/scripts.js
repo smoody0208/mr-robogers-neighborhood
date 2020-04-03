80c3d740968214aabd11b2bf1307964843cb96b3
Spencer Moody <spencer.moody@outlook.com>
2020-04-03 16:15:10 -0700

    Add form field for name

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ function robogersNeighborhood(message) {
   var result = [];
   var beepBoop = ["beep", "boop", "won't you be my neighbor"];
   var num = [1, 2, 3];
+ 
+  var nameInput = $("#nameInput").val();
+  console.log(nameInput);
 
   if (message<=0) {
     alert("Please Enter a Positive Number");
@@ -11,13 +14,17 @@ function robogersNeighborhood(message) {
     
   }
 
+
   for(var i=0; i<= message; i++){
     if (i.toString().includes(num[2])) {
       result.push(" " + beepBoop[2]);
+    } else if (i % 3 === 0) {
+      result.push(beepBoop[2] + " " + nameInput);
     } else if (i.toString().includes(num[1])) {
       result.push(" " + beepBoop[1]);
     } else if (i.toString().includes(num[0])) {
       result.push(" " + beepBoop[0]);
+
     } else {
       result.push(" " + i);
     }
@@ -27,11 +34,13 @@ function robogersNeighborhood(message) {
 
 //Front-end:
 
+
 $(document).ready(function(){
   $("form#robogers").submit(function(event){
     event.preventDefault();
     var message = parseInt($("input#messageInput").val());
     var output = robogersNeighborhood(message);
+    
     
     
     $("#result").text(output);
