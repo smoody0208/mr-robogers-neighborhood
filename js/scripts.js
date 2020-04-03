d728bb509bdb74be1b7410989bcfe299a0d8e837
Spencer Moody <spencer.moody@outlook.com>
2020-04-03 09:05:10 -0700

    Add some backend logic

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function robogersNeighborhood(message) {
+  var result = [];
+
+  if (number<0) {
+    alert("Please Enter a Positive Number");
+    return "";
+  }
+
+  for(var i = 0; i<= message; i++){
+    if (i === 3) {
+      result.push("won't you be my neighbor");
+    } else if (i === )
+  }
+}
